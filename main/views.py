dde58e2fbaf787a4ac3a4f97a159e63daaf71f32
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-04-27 20:16:28 +0300

    create userView and craete user fuc

--- main/views.py
+++ main/views.py
@@ -9,3 +9,23 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+
+
+
+class UserView(APIView):
+    """Create a new user"""
+    def post(self, request):
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        first_name = request.POST.get('first_name',None)
+        try:
+            user = User.objects.create(
+                username=username,
+                password=make_password(password),
+                first_name=first_name
+            )
+            user.save()
+            token,created = Token.objects.get_or_create(user=user)  
+            return Response({'token':token.key},status=status.HTTP_201_CREATED)
+        except:
+            return Response({'status':'The username already exist'},status=status.HTTP_400_BAD_REQUEST)
