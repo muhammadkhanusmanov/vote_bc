2b02bce827dd95f069ad24186aa6cf191544fa32
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-04-27 21:32:30 +0300

    poll view

--- main/views.py
+++ main/views.py
@@ -10,6 +10,10 @@ from django.contrib.auth.hashers import make_password
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
 
+from .models import Polls
+
+from .serialzer import PollSerializer 
+
 
 
 class UserView(APIView):
@@ -51,4 +55,37 @@ class UserLogin(models.Model):
         user = request.user
         token = Token.objects.get_or_create(user=user)
         return Response({'status':True,'token':token.key},status=status.HTTP_200_OK)
-        
+
+class PollView(APIView):
+    '''Create a new poll'''
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+    """Create a new poll"""
+    def post(self, request):
+        data = request.data
+        try:
+            poll = Polls.objects.create(
+                poll_que=data.get('poll_que', None),
+                que1=data.get('que1', None),
+                que2=data.get('que2', None),
+                que3=data.get('que3', None),
+                que4=data.get('que4', None),
+                que5=data.get('que5', None),
+                que6=data.get('que6', None),
+                que7=data.get('que7', None),
+                time=data.get('time', None)
+            )
+            poll.save()
+            return Response({'status':True},status=status.HTTP_201_CREATED)
+        except:
+            return Response({'status':False},status=status.HTTP_400_BAD_REQUEST)
+
+    '''delete the poll'''
+    def delete(self, request,id:str):
+        try:
+            poll = Polls.objects.get(id=id)
+            poll.delete()
+            return Response({'status':True},status=status.HTTP_200_OK)
+        except:
+            return Response({'status':False},status=status.HTTP_400_BAD_REQUEST)
+
