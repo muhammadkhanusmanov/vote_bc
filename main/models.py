d507529f888f05e11cbca4fa4e2ce9f86c8bfa7e
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-04-27 21:32:42 +0300

    model

--- main/models.py
+++ main/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Candidate(models.Model):
-    poll_que = models.CharField(max_length=300)
-    candidates = models.ManyToManyField(User,related_name='candidate')
-
-    def __str__(self):
-        return f'{self.poll_que[:12]}...'
 
 class Poll(models.Model):
     poll_que = models.CharField(max_length=300)
@@ -17,3 +11,6 @@ class Poll(models.Model):
     que5 = models.CharField(max_length=150, blank=True, null=True, default=None)
     que6 = models.CharField(max_length=150, blank=True, null=True, default=None)
     que7 = models.CharField(max_length=150, blank=True, null=True, default=None)
+
+    def __str__(self):
+        return self.poll_que
