c8c9e77445d73cda5b68a51cd281ee6646c9f678
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-04-27 21:55:30 +0300

    vote model

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Poll(models.Model):
+class Polls(models.Model):
     poll_que = models.CharField(max_length=300)
     que1 = models.CharField(max_length=150, blank=True, null=True, default=None)
     que2 = models.CharField(max_length=150, blank=True, null=True, default=None)
@@ -11,6 +11,17 @@ class Poll(models.Model):
     que5 = models.CharField(max_length=150, blank=True, null=True, default=None)
     que6 = models.CharField(max_length=150, blank=True, null=True, default=None)
     que7 = models.CharField(max_length=150, blank=True, null=True, default=None)
+    time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.poll_que
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Polls, on_delete=models.CASCADE,related_name='votes')
+    answer = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.user.username
+
