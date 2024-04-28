8ffa42160c7b5ef922aa73f0f52fa410100b3b12
muhammadkhanusmanov <uqaxromon@gmail.com>
2024-04-28 17:13:50 +0300

    register

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Polls,Vote
+from .models import Polls,Vote,Hujjat
 
 
-admin.site.register([Polls,Vote])
+admin.site.register(
+    [Polls,Vote,Hujjat]
+)
